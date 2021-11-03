b7aa2e5cbd9a9355bfe7a28bee512c8f31709a39
redaphid <iam@aaronherres.com>
2021-11-03 16:03:04 -0700

    adding some frame characterization jic we want it for the output

---
+++ src/frame.test.ts
@@ -0,0 +1,38 @@
+import { characterizeFrame, FrameType, scoreFrame } from "./frame"
+describe("Frame", () => {
+  describe("Characterizing a Frame", () => {
+    let frameType: FrameType | undefined
+    describe("When a frame has no shots yet", () => {
+      beforeEach(() => {
+        frameType = characterizeFrame([])
+      })
+      it("should be a open frame", () => {
+        expect(frameType).toBe(FrameType.Open)
+      })
+    })
+    describe("When a frame has 1 shot, and it isn't a strike", () => {
+      beforeEach(() => {
+        frameType = characterizeFrame([5])
+      })
+      it("should be a open frame", () => {
+        expect(frameType).toBe(FrameType.Open)
+      })
+    })
+    describe("When a frame has 10 pins on the first shot", () => {
+      beforeEach(() => {
+        frameType = characterizeFrame([10])
+      })
+      it("should be a strike frame", () => {
+        expect(frameType).toBe(FrameType.Strike)
+      })
+    })
+    describe("When a frame has 10 pins on the second shot", () => {
+      beforeEach(() => {
+        frameType = characterizeFrame([6, 4])
+      })
+      it("should be a spare frame", () => {
+        expect(frameType).toBe(FrameType.Spare)
+      })
+    })
+  })
+})
