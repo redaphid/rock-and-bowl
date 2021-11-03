16488b0e4901b49edd0abd6b8211114c800b39ef
redaphid <iam@aaronherres.com>
2021-11-03 16:42:09 -0700

    refactoring/redescribing tests

--- src/Frame.test.ts
+++ src/Frame.test.ts
@@ -1,35 +1,41 @@
 import { characterizeFrame, FrameType } from "./Frame"
 describe("Frame", () => {
-  describe("Characterizing a Frame", () => {
+  describe("When characterizing a Frame", () => {
     let frameType: FrameType | undefined
     let rolls: number[] | undefined
-    describe("When a frame has no shots yet", () => {
-      beforeEach(() => {
-        rolls = []
-        frameType = characterizeFrame(rolls)
-      })
-      it("should be incomplete", () => {
-        expect(frameType).toBe(FrameType.Incomplete)
-      })
-    })
-    describe("when the frame has 2 shots, and the sum of the 2 <10", ()=>{
-      beforeEach(() => {
-        rolls = [5, 2]
-        frameType = characterizeFrame(rolls)
-      })
-      it("should be a open frame", () => {
-        expect(frameType).toBe(FrameType.Open)
-      })
-    })
-    describe("When a frame has 1 shot, and it isn't a strike", () => {
-      beforeEach(() => {
-        rolls = [5]
-        frameType = characterizeFrame(rolls)
-      })
-      it("should be a open frame", () => {
-        expect(frameType).toBe(FrameType.Incomplete)
+    describe("Incomplete frames", () => {
+      describe("When the player hasn't rolled the ball yet", () => {
+        beforeEach(() => {
+          rolls = []
+          frameType = characterizeFrame(rolls)
+        })
+        it("should be incomplete", () => {
+          expect(frameType).toBe(FrameType.Incomplete)
+        })
+        describe
+        describe("When the player rolled the ball once, and it isn't a strike", () => {
+          beforeEach(() => {
+            rolls = [5]
+            frameType = characterizeFrame(rolls)
+          })
+          it("should be an incomplete frame", () => {
+            expect(frameType).toBe(FrameType.Incomplete)
+          })
+        })
       })
     })
+    describe("Open frames", ()=>{
+      describe("when the frame has 2 shots, and the sum of the 2 <10", () => {
+        beforeEach(() => {
+          rolls = [5, 2]
+          frameType = characterizeFrame(rolls)
+        })
+        it("should be a open frame", () => {
+          expect(frameType).toBe(FrameType.Open)
+        })
+      })
+    })
+
     describe("When a frame has 10 pins on the first shot", () => {
       beforeEach(() => {
         rolls = [10]
@@ -39,7 +45,7 @@ describe("Frame", () => {
         expect(frameType).toBe(FrameType.Strike)
       })
     })
-    describe("When a frame has 10 pins on the second shot", () => {
+    describe("When a frame has a sum 10 pins on the second shot", () => {
       beforeEach(() => {
         rolls = [6, 4]
         frameType = characterizeFrame(rolls)
