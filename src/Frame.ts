50dd734042203c4c3246260ac68259e2a1d9e44b
redaphid <iam@aaronherres.com>
2021-11-03 15:59:57 -0700

    frame interfaces. Trying out this data-driven design that's so hot right now

---
+++ src/Frame.ts
@@ -0,0 +1,11 @@
+
+
+//*technically* this could be done with an array of arrays, 
+//but I'm putting them in properties for clarity.
+interface Frame {
+  rolls: number[];
+}
+
+interface Game {
+  frames: Frame[];
+}
