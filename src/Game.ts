87f10b02f1db248e0ec22f3169b0ff7c08d7336c
redaphid <iam@aaronherres.com>
2021-11-03 18:36:37 -0700

    ugly interface to library

--- src/Game.ts
+++ src/Game.ts
@@ -1,5 +1,19 @@
 import { FrameInfo } from "./Frame"
+import bowl from "bowling"
 
-export const scoreGame = (frames:FrameInfo[])=>{
-  return 0
+
+// var aCompleteGame = ['81','9-','9/','71','9-','X','90','70','x','7-'],
+//     result
+
+// result = bowl(aCompleteGame)
+
+// console.log(result)
+export const scoreGame = (frames: FrameInfo[]) => {
+  const scoreResults = bowl(['0'])
+  return scoreResults.slice(-1)[0].cumulative
+  const rolls = frames.map((f) => f.rolls)
+
+  return rolls.reduce((score, roll) => {
+    return score + roll[0]
+  }, 0)
 }
