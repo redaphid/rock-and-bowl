fb49f2b20d555e0be4669aa4f9fc781e201b8c06
redaphid <iam@aaronherres.com>
2021-11-03 18:12:03 -0700

    constant->variable

--- src/Score.test.ts
+++ src/Score.test.ts
@@ -6,27 +6,42 @@ describe("When scoring a game", () => {
     expect(scoreGame).toBeDefined()
   })
 
-  describe("When the player hasn't even rolled the ball", ()=>{
-      beforeEach(()=>{
-        const frames = []
-        score = scoreGame([])
-      })
-      it("should give them a score of 0", ()=>{
-        expect(score).toEqual(0)
-      })
+  describe("When the player hasn't even rolled the ball", () => {
+    beforeEach(() => {
+      const frames = []
+      score = scoreGame([])
+    })
+    it("should give them a score of 0", () => {
+      expect(score).toEqual(0)
+    })
   })
-  describe("When the player hasn't completed any frames", ()=>{
-    beforeEach(()=>{
+  describe("When the player hasn't completed any frames", () => {
+    beforeEach(() => {
       const frames = [
         {
           status: FrameStatus.Incomplete,
-          rolls:[2]
-        }
+          rolls: [2],
+        },
       ]
       score = scoreGame(frames)
     })
-    it("should give them a score of 0", ()=>{
+    it("should give them a score of 0", () => {
       expect(score).toEqual(0)
     })
-})
+    describe("When the player completed a single frame, hitting 7 pins down", () => {
+      beforeEach(() => {
+        const frames = [
+          {
+            status: FrameStatus.Incomplete,
+            rolls: [3, 4],
+          },
+        ]
+        score = scoreGame(frames)
+      })
+
+      it("should give them a score of 7", () => {
+        expect(score).toEqual(7)
+      })
+    })
+  })
 })
