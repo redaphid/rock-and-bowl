1c543e38a4e5c8090111bdff72aa3aad9f375129
redaphid <iam@aaronherres.com>
2021-11-03 19:00:21 -0700

    spot checking wikipedia examples

--- src/Score.test.ts
+++ src/Score.test.ts
@@ -36,5 +36,45 @@ describe("When scoring a game", () => {
         expect(score).toEqual(7)
       })
     })
+    describe("When the player completed a single frame, hitting 7 pins down", () => {
+      beforeEach(() => {
+        const frames = [
+         [3,4]
+        ]
+        score = scoreGame(frames)
+      })
+
+      it("should give them a score of 7", () => {
+        expect(score).toEqual(7)
+      })
+    })
+    
+    //Spot checking with the wikipedia examples
+    describe("When a player gets a strike on the first frame, and knocks down 9 pins total in the 2nd", () => {
+      let frames: number[][]
+      beforeEach(() => {  
+        frames = [
+          [10],
+          [3,6]
+        ]
+        score = scoreGame(frames)
+      })
+      it("should give them a score of 28", ()=>{
+        expect(score).toEqual(28)
+      })
+    })
+    describe("When a player gets a spare after knocking down 7 pins, then knocks down 6 the next frame", () => {
+      let frames: number[][]
+      beforeEach(() => {  
+        frames = [
+          [7,3],
+          [4,2]
+        ]
+        score = scoreGame(frames)
+      })
+      it("should give them a score of 20", ()=>{
+        expect(score).toEqual(20)
+      })
+    })
   })
 })
